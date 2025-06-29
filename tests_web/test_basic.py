b1107625b7962a9a22571325a38c6621b21eeec8
Rohan Panchal <rohanpanchal76@gmail.com>
2025-06-29 20:08:59 +0000

    chromedriver fix

--- tests_web/test_basic.py
+++ tests_web/test_basic.py
@@ -13,7 +13,7 @@ def test_textbox():
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--headless=new")
 
-    # chromedriver_autoinstaller.install()
+    chromedriver_autoinstaller.install()
 
     driver = webdriver.Chrome(service=ChromeService(), options=chrome_options)
     driver.get("https://demoqa.com/text-box")
